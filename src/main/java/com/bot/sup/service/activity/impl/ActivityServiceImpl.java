537d3fc239c764929962fb099dbbe26381d68430
Ruslan N <>
2022-10-10 10:48:22 +0300

    refactor service

--- src/main/java/com/bot/sup/service/activity/impl/ActivityServiceImpl.java
+++ src/main/java/com/bot/sup/service/activity/impl/ActivityServiceImpl.java
@@ -5,6 +5,8 @@ import com.bot.sup.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @RequiredArgsConstructor
 public class ActivityService {
@@ -13,4 +15,9 @@ public class ActivityService {
     public void save(Activity activity) {
         activityRepository.save(activity);
     }
+
+    public Activity findById(Long id) {
+        return activityRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
 }
