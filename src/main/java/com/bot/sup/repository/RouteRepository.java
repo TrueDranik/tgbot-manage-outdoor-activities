a4d580a52109397609b6096b35934c7f39f3c971
Ruslan N <>
2022-10-18 22:54:02 +0300

    edit repository

--- src/main/java/com/bot/sup/repository/RouteRepository.java
+++ src/main/java/com/bot/sup/repository/RouteRepository.java
@@ -1,9 +1,9 @@
 package com.bot.sup.repository;
 
-import com.bot.sup.model.entity.Activity;
+import com.bot.sup.model.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RouteRepository extends JpaRepository<Activity, Long> {
+public interface RouteRepository extends JpaRepository<Route, Long> {
 }
