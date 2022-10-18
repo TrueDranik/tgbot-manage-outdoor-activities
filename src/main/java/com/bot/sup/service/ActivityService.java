68a8c5260311d491e5a7191123e26d9565b7d05e
Ruslan N <>
2022-10-18 22:54:51 +0300

    edit repository

--- src/main/java/com/bot/sup/service/ActivityService.java
+++ src/main/java/com/bot/sup/service/ActivityService.java
@@ -1,16 +1,16 @@
 package com.bot.sup.service;
 
 import com.bot.sup.model.entity.Route;
-import com.bot.sup.repository.ActivityRepository;
+import com.bot.sup.repository.RouteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ActivityService {
-    private final ActivityRepository activityRepository;
+    private final RouteRepository routeRepository;
 
     public void save(Route route) {
-        activityRepository.save(route);
+        routeRepository.save(route);
     }
 }
