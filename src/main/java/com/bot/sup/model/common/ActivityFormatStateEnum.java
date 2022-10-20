168a129758ef62ecb8cc8b6eaace80958501aa5f
Ruslan N <>
2022-10-20 19:35:54 +0300

    added new states

---
+++ src/main/java/com/bot/sup/model/common/ActivityFormatStateEnum.java
@@ -0,0 +1,7 @@
+package com.bot.sup.model.common;
+
+public enum ActivityFormatStateEnum {
+    ASK_ACTIVITY_FORMAT_NAME,
+    FILLING_ACTIVITY_FORMAT,
+    REGISTERED_ACTIVITY_FORMAT
+}
---
+++ src/main/java/com/bot/sup/model/common/ActivityTypeStateEnum.java
@@ -0,0 +1,7 @@
+package com.bot.sup.model.common;
+
+public enum ActivityTypeStateEnum {
+    ASK_ACTIVITY_TYPE_NAME,
+    FILLING_ACTIVITY_TYPE,
+    REGISTERED_ACTIVITY_TYPE
+}
