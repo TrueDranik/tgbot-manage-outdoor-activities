6a1915d51a1f7a9c107f265f40a3cb6b12b6ac20
Ruslan N <>
2022-10-20 19:34:16 +0300

    update

--- src/main/java/com/bot/sup/repository/ActivityTypeRepository.java
+++ src/main/java/com/bot/sup/repository/ActivityTypeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ActivityTypeRepository extends JpaRepository<ActivityType, Long> {
+    boolean existsByName(String userAnswer);
 }
