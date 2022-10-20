513ba0c108e18f671840cf0b7dddd8c1da433513
Ruslan N <>
2022-10-20 19:35:26 +0300

    added new cache

---
+++ src/main/java/com/bot/sup/cache/ActivityFormatDataCache.java
@@ -0,0 +1,25 @@
+package com.bot.sup.cache;
+
+import com.bot.sup.model.common.ActivityFormatStateEnum;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+@Component
+public class ActivityFormatDataCache {
+    private final Map<Long, ActivityFormatStateEnum> activityFormatState = new WeakHashMap<>();
+    private final Map<Long, Long> activityFormatForUpdate = new WeakHashMap<>();
+
+    public void setActivityFormatCurrentState(Long chatId, ActivityFormatStateEnum registrationState) {
+        activityFormatState.put(chatId, registrationState);
+    }
+
+    public ActivityFormatStateEnum getActivityFormatCurrentState(Long chatId) {
+        return activityFormatState.getOrDefault(chatId, ActivityFormatStateEnum.FILLING_ACTIVITY_FORMAT);
+    }
+
+    public Long getActivityFormatForUpdate(Long chatId) {
+        return activityFormatForUpdate.get(chatId);
+    }
+}
