b3352be7213f29e987ac10e57c1501ed5ddc545f
Ruslan N <>
2022-10-21 03:15:40 +0300

    edited DTO

--- src/main/java/com/bot/sup/model/dto/ActivityCreateDto.java
+++ src/main/java/com/bot/sup/model/dto/ActivityCreateDto.java
@@ -1,15 +1,20 @@
 package com.bot.sup.model.dto;
 
+import com.bot.sup.model.entity.ActivityFormat;
+import com.bot.sup.model.entity.ActivityType;
 import com.bot.sup.model.entity.Route;
 import lombok.Data;
 
 @Data
 public class ActivityCreateDto {
-    private String name;
-    private String startPointCoordinates;
-    private String startPointName;
-    private String finishPointCoordinates;
-    private String finishPointName;
-    private String mapLink;
+    private Long id;
+    private String seasonality;
+    private ActivityFormat activityFormat;
+    private ActivityType activityType;
+    private String description;
     private Route route;
+    private String duration;
+    private String age;
+    private String complexity;
+    private String price;
 }
