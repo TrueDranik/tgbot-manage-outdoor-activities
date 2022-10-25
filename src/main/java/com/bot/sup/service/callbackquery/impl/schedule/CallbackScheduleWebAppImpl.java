fecb159834d0b561bee50b7a0e6025ee34ba208c
Ruslan N <>
2022-10-25 18:23:43 +0300

    [BOTS-207] created new callback class

---
+++ src/main/java/com/bot/sup/service/callbackquery/impl/schedule/CallbackScheduleWebAppImpl.java
@@ -0,0 +1,76 @@
+package com.bot.sup.service.callbackquery.impl.schedule;
+
+import com.bot.sup.common.enums.CallbackEnum;
+import com.bot.sup.common.properties.message.MainMessageProperties;
+import com.bot.sup.service.callbackquery.Callback;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.webapp.WebAppInfo;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class CallbackScheduleWebAppImpl implements Callback {
+    private final MainMessageProperties mainMessageProperties;
+
+    private static final Set<CallbackEnum> ACTIVITIES = Set.of(CallbackEnum.SCHEDULE_WEBAPP);
+
+    @Override
+    public BotApiMethod<?> getCallbackQuery(CallbackQuery callbackQuery) throws TelegramApiException {
+        return EditMessageText.builder()
+                .messageId(callbackQuery.getMessage().getMessageId())
+                .chatId(callbackQuery.getMessage().getChatId())
+                .text("Добавить тур/составить расписание")
+                .replyMarkup(createInlineKeyboard())
+                .build();
+    }
+
+    private InlineKeyboardMarkup createInlineKeyboard() {
+        WebAppInfo webAppInfo = WebAppInfo.builder()
+                .url("https://192.168.1.35:3000")
+                .build();
+
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+
+        buttons.add(List.of(InlineKeyboardButton.builder()
+                .text("Добавить новый маршрут")
+                .webApp(new WebAppInfo("https://192.168.1.35:3000/make"))
+                .build()));
+        buttons.add(List.of(InlineKeyboardButton.builder()
+                .text("Составить расписание в конструкторе")
+                .webApp(webAppInfo)
+                .build()));
+        buttons.add(List.of(InlineKeyboardButton.builder()
+                .text("Создать новую активность")
+                .webApp(webAppInfo)
+                .build()));
+        buttons.add(List.of(InlineKeyboardButton.builder()
+                .text("Изменить существующую активность")
+                .webApp(webAppInfo)
+                .build()));
+
+        buttons.add(List.of(InlineKeyboardButton.builder()
+                .text(mainMessageProperties.getBack())
+                .callbackData("SCHEDULE")
+                .build()));
+
+        return InlineKeyboardMarkup.builder()
+                .keyboard(buttons)
+                .build();
+    }
+
+    @Override
+    public Collection<CallbackEnum> getSupportedActivities() {
+        return ACTIVITIES;
+    }
+}
