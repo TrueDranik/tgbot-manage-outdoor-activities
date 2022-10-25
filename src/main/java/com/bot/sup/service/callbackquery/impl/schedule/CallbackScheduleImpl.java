cc71152761a7a9c12c4d338bbda9ff30a9e96b0f
Ruslan N <>
2022-10-25 17:22:18 +0300

    [BOTS-206] created callback Schedule

---
+++ src/main/java/com/bot/sup/service/callbackquery/impl/schedule/CallbackScheduleImpl.java
@@ -0,0 +1,62 @@
+package com.bot.sup.service.callbackquery.impl.schedule;
+
+import com.bot.sup.common.enums.CallbackEnum;
+import com.bot.sup.common.properties.message.MainMessageProperties;
+import com.bot.sup.service.callbackquery.Callback;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class CallbackScheduleImpl implements Callback {
+    private final MainMessageProperties mainMessageProperties;
+
+    private static final Set<CallbackEnum> ACTIVITIES = Set.of(CallbackEnum.SCHEDULE);
+
+    @Override
+    public BotApiMethod<?> getCallbackQuery(CallbackQuery callbackQuery) throws TelegramApiException {
+        return EditMessageText.builder()
+                .messageId(callbackQuery.getMessage().getMessageId())
+                .chatId(callbackQuery.getMessage().getChatId())
+                .text("Меню расписания")
+                .replyMarkup(createInlineKeyboard())
+                .build();
+    }
+
+    private InlineKeyboardMarkup createInlineKeyboard() {
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+
+        buttons.add(List.of(InlineKeyboardButton.builder()
+                .text("Список расписания для форматов")
+                .callbackData("LIST_SCHEDULE")
+                .build()));
+        buttons.add(List.of(InlineKeyboardButton.builder()
+                .text("Добавить тур/составить расписание")
+                .callbackData("?")
+                .build()));
+        buttons.add(List.of(InlineKeyboardButton.builder()
+                .text(mainMessageProperties.getMenu())
+                .callbackData("MENU")
+                .build()));
+
+        return InlineKeyboardMarkup.builder()
+                .keyboard(buttons)
+                .build();
+    }
+
+    @Override
+    public Collection<CallbackEnum> getSupportedActivities() {
+        return ACTIVITIES;
+    }
+}
