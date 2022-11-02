0aebbc53553f7b2aad5f4699324c204ab050e1d5
Ruslan N <>
2022-11-02 16:20:21 +0300

    [BOTS-199] created activity service

--- src/main/java/com/bot/sup/service/activity/impl/ActivityServiceImpl.java
+++ src/main/java/com/bot/sup/service/activity/impl/ActivityServiceImpl.java
@@ -3,10 +3,11 @@ package com.bot.sup.service.activity.impl;
 import com.bot.sup.mapper.ActivityMapper;
 import com.bot.sup.model.dto.ActivityCreateDto;
 import com.bot.sup.model.entity.Activity;
-import com.bot.sup.model.entity.Route;
 import com.bot.sup.repository.ActivityRepository;
 import com.bot.sup.service.activity.ActivityService;
-import com.bot.sup.service.route.impl.RouteServiceImpl;
+import com.bot.sup.service.activity.format.ActivityFormatService;
+import com.bot.sup.service.activity.type.ActivityTypeService;
+import com.bot.sup.service.route.RouteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,9 @@ import java.util.List;
 public class ActivityServiceImpl implements ActivityService {
     private final ActivityRepository activityRepository;
     private final ActivityMapper activityMapper;
-    private final RouteServiceImpl routeService;
+    private final RouteService routeService;
+    private final ActivityFormatService activityFormatService;
+    private final ActivityTypeService activityTypeService;
 
     public void save(Activity activity) {
         activityRepository.save(activity);
@@ -26,29 +29,55 @@ public class ActivityServiceImpl implements ActivityService {
 
     @Override
     public List<Activity> getAllActivity() {
-        return null;
+        List<Activity> activities = activityRepository.findAll();
+
+        if (activities.isEmpty()) {
+            throw new EntityNotFoundException("Activities not found");
+        }
+
+        return activities;
     }
 
     @Override
     public Activity getActivityById(Long id) {
-        return activityRepository.findById(id)
-                .orElseThrow(EntityNotFoundException::new);
+        return findActivityById(id);
     }
 
     @Override
-    public void createActivity(ActivityCreateDto createDto) {
+    public Activity createActivity(ActivityCreateDto createDto) {
         Activity activity = activityMapper.dtoToDomain(createDto);
 
-        activityRepository.save(activity);
+        activity.setActivityFormat(activityFormatService.getActivityFormatById(createDto.getActivityFormatId()));
+        activity.setActivityType(activityTypeService.getActivityTypeById(createDto.getActivityTypeId()));
+        activity.setRoute(routeService.getRouteById(createDto.getRouteId()));
+
+        return activityRepository.save(activity);
     }
 
     @Override
-    public Activity updateActivity(Long id, Activity activity) {
-        return null;
+    public Activity updateActivity(Long id, ActivityCreateDto activityCreateDto) {
+        Activity activityById = findActivityById(id);
+        activityById.setName(activityCreateDto.getName());
+        activityById.setSeasonality(activityCreateDto.getSeasonality());
+        activityById.setActivityFormat(activityFormatService.getActivityFormatById(activityCreateDto.getActivityFormatId()));
+        activityById.setActivityType(activityTypeService.getActivityTypeById(activityCreateDto.getActivityTypeId()));
+        activityById.setDescription(activityCreateDto.getDescription());
+        activityById.setRoute(routeService.getRouteById(activityCreateDto.getRouteId()));
+        activityById.setDuration(activityCreateDto.getDuration());
+        activityById.setAge(activityCreateDto.getAge());
+        activityById.setComplexity(activityCreateDto.getComplexity());
+        activityById.setPrice(activityCreateDto.getPrice());
+
+        return activityRepository.save(activityById);
     }
 
     @Override
     public void deleteActivity(Long id) {
+        activityRepository.delete(findActivityById(id));
+    }
 
+    private Activity findActivityById(Long id) {
+        return activityRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Activity with id[" + id + "] not found"));
     }
 }
