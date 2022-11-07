f925e9a97cd511ffcbb0b83fff49f5ef7944f852
Ruslan N <>
2022-11-07 13:38:48 +0300

    [BOTS-214] created new querys

--- src/main/java/com/bot/sup/repository/BookingRepository.java
+++ src/main/java/com/bot/sup/repository/BookingRepository.java
@@ -2,8 +2,13 @@ package com.bot.sup.repository;
 
 import com.bot.sup.model.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    @Query("SELECT b FROM Booking b WHERE b.client.id = ?1")
+    Optional<Booking> invitedUsers(Long clientId);
 }
