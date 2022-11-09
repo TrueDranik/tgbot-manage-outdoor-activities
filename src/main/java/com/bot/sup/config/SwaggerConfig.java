ce04251ba0caccccac56cab0b6fc1a76a9bc686a
Ruslan N <>
2022-11-09 17:23:06 +0300

    fixed

--- src/main/java/com/bot/sup/config/SwaggerConfig.java
+++ src/main/java/com/bot/sup/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("https://tgsupbot.reliab.tech/")
+                .host("https://tgsupbot.reliab.tech")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
