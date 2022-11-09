e283376728729c3f3dbfca77228d0802d65041fd
Ruslan N <>
2022-11-09 15:27:01 +0300

    addded basePackage

--- src/main/java/com/bot/sup/api/SwaggerConfig.java
+++ src/main/java/com/bot/sup/api/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.bot.sup.api.rest"))
                 .paths(PathSelectors.any())
                 .build();
     }
