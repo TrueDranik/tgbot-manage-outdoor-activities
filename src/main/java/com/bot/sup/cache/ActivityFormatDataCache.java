5660256a7efba0e067fa1c02a6ea6398aad9be23
Ruslan N <>
2022-11-14 17:45:43 +0300

    [BOTS-228] edited cache

--- src/main/java/com/bot/sup/cache/ActivityFormatDataCache.java
+++ src/main/java/com/bot/sup/cache/ActivityFormatDataCache.java
@@ -1,6 +1,7 @@
 package com.bot.sup.cache;
 
 import com.bot.sup.common.enums.ActivityFormatStateEnum;
+import com.bot.sup.model.entity.ActivityFormat;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -9,6 +10,7 @@ import java.util.WeakHashMap;
 @Component
 public class ActivityFormatDataCache {
     private final Map<Long, ActivityFormatStateEnum> activityFormatState = new WeakHashMap<>();
+    private final Map<Long, ActivityFormat> activityFormatData = new WeakHashMap<>();
     private final Map<Long, Long> activityFormatForUpdate = new WeakHashMap<>();
 
     public void setActivityFormatCurrentState(Long chatId, ActivityFormatStateEnum registrationState) {
@@ -17,6 +19,26 @@ public class ActivityFormatDataCache {
 
     public ActivityFormatStateEnum getActivityFormatCurrentState(Long chatId) {
         return activityFormatState.getOrDefault(chatId, ActivityFormatStateEnum.FILLING_ACTIVITY_FORMAT);
+    }
+
+    public void removeActivityFormatCurrentState(Long chatId) {
+        activityFormatState.remove(chatId);
+    }
+
+    public ActivityFormat getActivityFormatProfileData(Long chatId) {
+        return activityFormatData.getOrDefault(chatId, new ActivityFormat());
+    }
+
+    public void saveActivityFormatProfileData(Long chatId, ActivityFormat activityFormat) {
+        activityFormatData.put(chatId, activityFormat);
+    }
+
+    public void saveActivityFormatForUpdate(Long chatId, Long activityFormatId) {
+        activityFormatForUpdate.put(chatId, activityFormatId);
+    }
+
+    public void removeActivityFormatForUpdate(Long chatId) {
+        activityFormatForUpdate.remove(chatId);
     }
 
     public Long getActivityFormatForUpdate(Long chatId) {
