0f15d9a479cc5aac27da801a29a175b25c2187f2
Ruslan N <>
2022-11-15 16:39:58 +0300

    [BOTS-231] Update RouteController

--- src/main/java/com/bot/sup/api/rest/RouteController.java
+++ src/main/java/com/bot/sup/api/rest/RouteController.java
@@ -4,13 +4,15 @@ import com.bot.sup.model.dto.RouteCreateDto;
 import com.bot.sup.model.entity.Route;
 import com.bot.sup.repository.RouteRepository;
 import com.bot.sup.service.route.RouteService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,32 +20,18 @@ import java.util.Optional;
 @CrossOrigin("*")
 @RequestMapping("/route")
 @RequiredArgsConstructor
+@Tag(name = "Маршрут")
 public class RouteController {
-    private final RouteRepository routeRepository;
     private final RouteService routeService;
 
     @GetMapping
+    @Operation(summary = "Получить список всех маршрутов")
     public ResponseEntity<List<Route>> getAllRoute(@RequestParam(required = false) Long id) {
-        try {
-            List<Route> routes = new ArrayList<>();
-
-            if (id == null) {
-                routes.addAll(routeRepository.findAll());
-            } else {
-                routes.addAll(routeRepository.findAllById(Collections.singleton(id)));
-            }
-
-            if (routes.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(routes, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return new ResponseEntity<>(routeService.getAllRoute(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получить маршрут по id")
     public ResponseEntity<Route> getRouteById(@PathVariable(name = "id") Long id) {
         try {
             Optional<Route> route = Optional.ofNullable(routeService.getRouteById(id));
@@ -58,33 +46,35 @@ public class RouteController {
         }
     }
 
-    //@CrossOrigin(value = "https://192.168.1.35:3000")
     @PostMapping
+    @Operation(summary = "Создать новый маршрут")
     public ResponseEntity<Route> createRoute(@RequestBody RouteCreateDto routeCreateDto) {
         try {
             return new ResponseEntity<>(routeService.createRoute(routeCreateDto), HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Изменить существующий маршрут")
     public ResponseEntity<Route> updateRoute(@PathVariable(name = "id") Long id, @RequestBody RouteCreateDto routeCreateDto) {
         try {
             return new ResponseEntity<>(routeService.updateRoute(id, routeCreateDto), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удалить маршрут по id")
     public ResponseEntity<Route> deleteRoute(@PathVariable(name = "id") Long id) {
         try {
             routeService.deleteRoute(id);
 
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
