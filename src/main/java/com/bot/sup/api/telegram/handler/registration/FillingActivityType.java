3d7caad7da2744e80b10f7cab08c2ae1c4d0df9f
Ruslan N <>
2022-11-15 15:21:03 +0300

    [BOTS-228] update filling activity

--- src/main/java/com/bot/sup/api/telegram/handler/registration/FillingActivityType.java
+++ src/main/java/com/bot/sup/api/telegram/handler/registration/FillingActivityType.java
@@ -6,8 +6,8 @@ import com.bot.sup.common.properties.message.ActivityMessageProperties;
 import com.bot.sup.common.properties.message.MainMessageProperties;
 import com.bot.sup.model.entity.ActivityType;
 import com.bot.sup.repository.ActivityTypeRepository;
-import com.bot.sup.service.activity.type.impl.ActivityTypeServiceImpl;
 import com.bot.sup.service.MessageService;
+import com.bot.sup.service.activity.type.impl.ActivityTypeServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -17,13 +17,14 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class FillingActivityType implements HandleRegistration{
+public class FillingActivityType implements HandleRegistration {
     private final MessageService messageService;
     private final ActivityTypeServiceImpl activityTypeServiceImpl;
     private final ActivityTypeDataCache activityTypeDataCache;
@@ -33,62 +34,87 @@ public class FillingActivityType implements HandleRegistration{
 
     @Override
     public BotApiMethod<?> getMessage(Message message) {
+        ActivityType activityType;
         Long chatId = message.getChatId();
+        Long activityForUpdateId = activityTypeDataCache.getActivityTypeForUpdate(chatId);
+        boolean forUpdate = activityForUpdateId != null;
 
-        if (activityTypeDataCache.getActivityTypeCurrentState(chatId).equals(ActivityTypeStateEnum.FILLING_ACTIVITY_TYPE))
-            activityTypeDataCache.setActivityTypeCurrentState(chatId, ActivityTypeStateEnum.ASK_ACTIVITY_TYPE_NAME);
+        if (forUpdate) {
+            activityType = activityTypeRepository.findById(activityForUpdateId)
+                    .orElseThrow(EntityNotFoundException::new);
 
-        return processInputMessage(message, chatId);
-    }
+            log.info("Found activity type with Id - {} and name - {}", activityType.getId(), activityType.getName());
+        } else {
+            activityType = activityTypeDataCache.getActivityTypeProfileData(chatId);
+        }
 
-    @Transactional
-    public BotApiMethod<?> processInputMessage(Message message, Long chatId){
-        BotApiMethod<?> replyToUser = null;
-        ActivityType activityType = new ActivityType();
-        String userAnswer = message.getText();
-        ActivityTypeStateEnum activityTypeCurrentState = activityTypeDataCache.getActivityTypeCurrentState(chatId);
+            if (activityTypeDataCache.getActivityTypeCurrentState(chatId).equals(ActivityTypeStateEnum.FILLING_ACTIVITY_TYPE))
+                activityTypeDataCache.setActivityTypeCurrentState(chatId, ActivityTypeStateEnum.ASK_ACTIVITY_TYPE_NAME);
 
-        if(activityTypeCurrentState.equals(ActivityTypeStateEnum.ASK_ACTIVITY_TYPE_NAME)){
-            replyToUser = messageService.buildReplyMessage(chatId, "Введите формат");
-            activityTypeDataCache.setActivityTypeCurrentState(chatId, ActivityTypeStateEnum.REGISTERED_ACTIVITY_TYPE);
+            return processInputMessage(message, chatId, activityType, forUpdate);
+        }
 
-            return replyToUser;
-        } else if (activityTypeCurrentState.equals(ActivityTypeStateEnum.REGISTERED_ACTIVITY_TYPE)) {
-            if (activityTypeRepository.existsByNameEqualsIgnoreCase(userAnswer)){
-                replyToUser = messageService.buildReplyMessage(chatId, "Данный формат существует");
+        @Transactional
+        public BotApiMethod<?> processInputMessage(Message message, Long chatId, ActivityType activityType,
+        boolean forUpdate){
+            BotApiMethod<?> replyToUser = null;
+            //ActivityType activityType = new ActivityType();
+            String userAnswer = message.getText();
+            ActivityTypeStateEnum activityTypeCurrentState = activityTypeDataCache.getActivityTypeCurrentState(chatId);
+
+            if (activityTypeCurrentState.equals(ActivityTypeStateEnum.ASK_ACTIVITY_TYPE_NAME)) {
+                replyToUser = messageService.buildReplyMessage(chatId, "Введите формат");
                 activityTypeDataCache.setActivityTypeCurrentState(chatId, ActivityTypeStateEnum.REGISTERED_ACTIVITY_TYPE);
 
                 return replyToUser;
+            } else if (activityTypeCurrentState.equals(ActivityTypeStateEnum.REGISTERED_ACTIVITY_TYPE)) {
+                if (activityTypeRepository.existsByNameEqualsIgnoreCase(userAnswer)) {
+                    replyToUser = messageService.buildReplyMessage(chatId, "Данный формат существует");
+                    activityTypeDataCache.setActivityTypeCurrentState(chatId, ActivityTypeStateEnum.REGISTERED_ACTIVITY_TYPE);
+
+                    return replyToUser;
+                }
+
+                try {
+                    activityType.setName(userAnswer);
+                } catch (IndexOutOfBoundsException e) {
+                    return messageService.buildReplyMessage(chatId, "Вы не ввели формат!");
+                }
+
+
+                if (forUpdate) {
+                    activityTypeDataCache.removeActivityTypeForUpdate(chatId);
+                }else {
+                    activityTypeServiceImpl.save(activityType);
+                }
+
+                replyToUser = messageService.getReplyMessageWithKeyboard(chatId, "Тип активности зарегистрирован", keyboardMarkup());
             }
 
-            try {
-                activityType.setName(userAnswer);
-            } catch (IndexOutOfBoundsException e){
-                return messageService.buildReplyMessage(chatId, "Вы не ввели формат!");
+            if (forUpdate) {
+                activityTypeServiceImpl.save(activityType);
+            } else {
+                activityTypeDataCache.saveActivityTypeProfileData(chatId, activityType);
             }
 
-            activityTypeServiceImpl.save(activityType);
-            replyToUser = messageService.getReplyMessageWithKeyboard(chatId, "Формат зарегистрирован", keyboardMarkup());
+            return replyToUser;
         }
 
-        return replyToUser;
-    }
+        private InlineKeyboardMarkup keyboardMarkup () {
+            List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
 
-    private InlineKeyboardMarkup keyboardMarkup(){
-        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+            buttons.add(List.of(
+                    InlineKeyboardButton.builder()
+                            .callbackData("SUP_ACTIVITY_TYPE")
+                            .text("Зарегистрировано")
+                            .build()));
+            return InlineKeyboardMarkup.builder()
+                    .keyboard(buttons)
+                    .build();
+        }
 
-        buttons.add(List.of(
-                InlineKeyboardButton.builder()
-                        .callbackData("SUP_ACTIVITY_TYPE")
-                        .text("Зарегистрировано")
-                        .build()));
-        return InlineKeyboardMarkup.builder()
-                .keyboard(buttons)
-                .build();
+        @Override
+        public Enum<?> getType () {
+            return ActivityTypeStateEnum.FILLING_ACTIVITY_TYPE;
+        }
     }
-
-    @Override
-    public Enum<?> getType() {
-        return ActivityTypeStateEnum.FILLING_ACTIVITY_TYPE;
-    }
-}
