12a815eb02a3e5fe8d296add75475fe9b4578482
Ruslan N <>
2022-11-15 18:14:08 +0300

    update routeController

--- src/main/java/com/bot/sup/api/rest/RouteController.java
+++ src/main/java/com/bot/sup/api/rest/RouteController.java
@@ -2,11 +2,8 @@ package com.bot.sup.api.rest;
 
 import com.bot.sup.model.dto.RouteCreateDto;
 import com.bot.sup.model.entity.Route;
-import com.bot.sup.repository.RouteRepository;
 import com.bot.sup.service.route.RouteService;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -33,48 +30,29 @@ public class RouteController {
     @GetMapping("/{id}")
     @Operation(summary = "Получить маршрут по id")
     public ResponseEntity<Route> getRouteById(@PathVariable(name = "id") Long id) {
-        try {
-            Optional<Route> route = Optional.ofNullable(routeService.getRouteById(id));
 
-            if (route.isPresent()) {
-                return new ResponseEntity<>(route.get(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        Optional<Route> route = Optional.ofNullable(routeService.getRouteById(id));
+
+        return route.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
     }
 
     @PostMapping
     @Operation(summary = "Создать новый маршрут")
     public ResponseEntity<Route> createRoute(@RequestBody RouteCreateDto routeCreateDto) {
-        try {
-            return new ResponseEntity<>(routeService.createRoute(routeCreateDto), HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return new ResponseEntity<>(routeService.createRoute(routeCreateDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     @Operation(summary = "Изменить существующий маршрут")
     public ResponseEntity<Route> updateRoute(@PathVariable(name = "id") Long id, @RequestBody RouteCreateDto routeCreateDto) {
-        try {
-            return new ResponseEntity<>(routeService.updateRoute(id, routeCreateDto), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return new ResponseEntity<>(routeService.updateRoute(id, routeCreateDto), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Удалить маршрут по id")
     public ResponseEntity<Route> deleteRoute(@PathVariable(name = "id") Long id) {
-        try {
-            routeService.deleteRoute(id);
-
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        routeService.deleteRoute(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
