a1b65b7926d5e5d8df4055511d06f297fb349a55
Ruslan N <>
2022-11-16 18:05:00 +0300

    update create dto

--- src/main/java/com/bot/sup/model/dto/ActivityCreateDto.java
+++ src/main/java/com/bot/sup/model/dto/ActivityCreateDto.java
@@ -3,12 +3,14 @@ package com.bot.sup.model.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
 @Schema(description = "Информация об активности")
 public class ActivityCreateDto {
     @Schema(description = "Name activity")
+    @Size(min = 0, max = 255)
     private String name;
     private String seasonality;
     private Long activityFormatId;
