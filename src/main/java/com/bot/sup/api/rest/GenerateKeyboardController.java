ad28fbc70ac7890d7666de7d26fdfdb421c29066
Ruslan N <>
2022-12-02 11:55:09 +0300

    added CORS policy

--- src/main/java/com/bot/sup/api/rest/GenerateKeyboardController.java
+++ src/main/java/com/bot/sup/api/rest/GenerateKeyboardController.java
@@ -2,12 +2,10 @@ package com.bot.sup.api.rest;
 
 import com.bot.sup.service.GenerateKeyboard;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/keyboard")
 @RequiredArgsConstructor
