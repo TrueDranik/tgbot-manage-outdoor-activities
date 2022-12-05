939234f21b1e8b411b2fb2a0c969ee11fbd38619
Ruslan N <>
2022-12-05 16:32:01 +0300

    refactored

--- src/main/java/com/bot/sup/api/rest/GenerateKeyboardController.java
+++ src/main/java/com/bot/sup/api/rest/GenerateKeyboardController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @CrossOrigin("*")
