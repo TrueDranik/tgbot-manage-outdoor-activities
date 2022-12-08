51ca429aee10fcd6723d5b314bd221c3827d2d6a
Ruslan N <>
2022-12-08 11:15:46 +0300

    [BOTS-312] the heir class has been changed

--- src/main/java/com/bot/sup/service/callbackquery/impl/schedule/CallbackScheduleToActivityFormatImpl.java
+++ src/main/java/com/bot/sup/service/callbackquery/impl/schedule/CallbackScheduleToActivityFormatImpl.java
@@ -9,7 +9,7 @@ import com.bot.sup.repository.ActivityFormatRepository;
 import com.bot.sup.service.callbackquery.Callback;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
@@ -33,7 +33,7 @@ public class CallbackScheduleToActivityFormatImpl implements Callback {
     public static final Set<CallbackEnum> ACTIVITIES = Set.of(SCHEDULE_TO_ACTIVITYFORMAT);
 
     @Override
-    public BotApiMethod<?> getCallbackQuery(CallbackQuery callbackQuery) {
+    public PartialBotApiMethod<?> getCallbackQuery(CallbackQuery callbackQuery) {
         List<ActivityFormat> activityFormats = activityFormatRepository.findAll();
         Long chatId = callbackQuery.getMessage().getChatId();
 
