d613d3739f82003d97f6d2e7ee8c9c90363de3eb
Ruslan N <>
2022-12-08 11:09:50 +0300

    [BOTS-312] create table "image_data"

---
+++ src/main/java/com/bot/sup/model/entity/ImageData.java
@@ -0,0 +1,31 @@
+package com.bot.sup.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "imageData")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    @Type(type="org.hibernate.type.BinaryType")
+    @Column(name = "imagedata")
+    private byte[] imageData;
+}
