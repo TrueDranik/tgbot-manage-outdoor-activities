5e1de380c402aab5279344494315f7bd986e15d9
Ruslan N <>
2022-12-14 11:45:14 +0300

    added image service

--- src/main/java/com/bot/sup/api/rest/ImageDataController.java
+++ src/main/java/com/bot/sup/api/rest/ImageDataController.java
@@ -1,8 +1,11 @@
 package com.bot.sup.api.rest;
 
+import com.bot.sup.model.dto.ImageDataCreateDto;
 import com.bot.sup.model.entity.ImageData;
 import com.bot.sup.service.files.ImageDataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +22,10 @@ public class ImageDataController {
     private final ImageDataService imageDataService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
-        imageDataService.uploadImage(file);
+    public ResponseEntity<ImageDataCreateDto> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+        ImageDataCreateDto imageDataCreateDto = imageDataService.uploadImage(file);
+
+        return new ResponseEntity<>(imageDataCreateDto, HttpStatus.OK);
     }
 
     @GetMapping("/info/{name}")
@@ -32,11 +37,26 @@ public class ImageDataController {
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<?> getImageByName(@PathVariable("name") String name) {
+    public ResponseEntity<byte[]> getImageByName(@PathVariable("name") String name) throws IOException {
         byte[] image = imageDataService.getImage(name);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.valueOf("image/png"))
                 .body(image);
+    }
+
+    @GetMapping("/download/{id}")
+    public ResponseEntity<ByteArrayResource> download(@PathVariable Long id) throws IOException {
+        ImageData image = imageDataService.getImageById(id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=%s".formatted(image.getName()));
+
+        ByteArrayResource resource = new ByteArrayResource(image.getImageData());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.parseMediaType(image.getType()))
+                .body(resource);
     }
 }
