572d9437bd2af460b3ca6ee0d43ae6b4dacdfae0
Ruslan N <>
2022-12-14 11:46:53 +0300

    TODO

--- src/main/java/com/bot/sup/mapper/ScheduleMapper.java
+++ src/main/java/com/bot/sup/mapper/ScheduleMapper.java
@@ -12,7 +12,7 @@ public interface ScheduleMapper extends BaseMapper<Schedule, ScheduleCreateDto> 
     Schedule toEntity(ScheduleDto scheduleDto);
 
     @Override
-    ScheduleCreateDto domainToDto(Schedule domain);
+    ScheduleCreateDto domainToDto(Schedule domain); //TODO: Удалить override методы, т.к точно такие уже были созданы
 
     @Override
     Schedule dtoToDomain(ScheduleCreateDto scheduleCreateDto);
