bf7bfd44f3a58937715575689369ee6b9b35babf
Alice Ostapova <alice.ostapova@gmail.com>
2023-01-12 10:42:56 +0300

    modified booking

--- src/main/java/com/bot/sup/model/entity/Booking.java
+++ src/main/java/com/bot/sup/model/entity/Booking.java
@@ -10,47 +10,44 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
 
+@Getter
 @Entity
 @Table(name = "booking")
-@Getter
 @Setter
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private Client client;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clientId")
+    private Client clientId;
 
-    @Column(name = "invited_users")
+    @Column(name= "invited_users")
     private int invitedUsers;
 
-    @BooleanFlag
-    @Column(name = "payment_status")
+    @Column(name= "invited_children")
+    private int invitedChildren;
+
+    @Column(name= "payment_status")
     private String paymentStatus;
 
-    @Column(name = "ins_time")
+    @Column(name= "ins_time")
     private LocalDate insTime;
 
-    @Column(name = "modif_time")
+    @Column(name= "modif_time")
     private LocalDate modifTime;
 
     @Column(name = "is_active")
-    private Boolean isActive;
+    private boolean isActive = true;
 
     @Column(name = "amount_paid")
-    private BigDecimal amount_paid;
+    private BigDecimal amountPaid;
 
     @Column(name = "payment_type")
     private String paymentType;
 
-    @ManyToMany
-    @JoinTable(
-            name = "schedule_booking",
-            joinColumns = @JoinColumn(name = "booking_id"),
-            inverseJoinColumns = @JoinColumn(name = "schedule_id")
-    )
-    @JsonManagedReference
-    private Set<Schedule> schedule;
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
 }
