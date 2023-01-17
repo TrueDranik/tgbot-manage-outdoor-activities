3f915c37091c28624efe04c39c3cdf5abc196a9a
Ruslan N <>
2023-01-17 14:35:00 +0300

    [BOTS-282] added new state

--- src/main/java/com/bot/sup/common/enums/InstructorStateEnum.java
+++ src/main/java/com/bot/sup/common/enums/InstructorStateEnum.java
@@ -1,11 +1,11 @@
 package com.bot.sup.common.enums;
 
 public enum InstructorStateEnum {
+    START_PROCESSING,
     ASK_FULL_NAME,
     FILLING_FULL_NAME,
     ASK_PHONE_NUMBER,
     ASK_TELEGRAM_ID,
     GET_TELEGRAM_ID,
-    REGISTERED_INSTRUCTOR,
     FILLING_INSTRUCTOR
 }
