9f3af7bcfb829d4871bb5302702a367f330aa5fc
Ruslan N <>
2023-01-17 14:37:01 +0300

    [BOTS-282] the return type has been changed

--- src/main/java/com/bot/sup/service/MessageService.java
+++ src/main/java/com/bot/sup/service/MessageService.java
@@ -1,7 +1,7 @@
 package com.bot.sup.service;
 
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
@@ -10,16 +10,16 @@ public class MessageService {
     public SendMessage buildReplyMessage(Long chatId, String text) {
         return SendMessage.builder()
                 .text(text)
-                .parseMode("Markdown")
+                .parseMode(ParseMode.MARKDOWN)
                 .chatId(chatId.toString())
                 .build();
     }
 
-    public BotApiMethod<?> getReplyMessageWithKeyboard(Long chatId, String text, InlineKeyboardMarkup keyboard) {
+    public SendMessage getReplyMessageWithKeyboard(Long chatId, String text, InlineKeyboardMarkup keyboard) {
         return SendMessage.builder()
                 .chatId(chatId.toString())
                 .text(text)
-                .parseMode("Markdown")
+                .parseMode(ParseMode.MARKDOWN)
                 .replyMarkup(keyboard)
                 .build();
     }
