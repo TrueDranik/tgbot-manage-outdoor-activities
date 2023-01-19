e2575050940ca949aee9bc1537b9fad0e1c0aec7
Alice Ostapova <alice.ostapova@gmail.com>
2023-01-19 14:32:09 +0300

    [BOTS-347] modified specifications controller.

--- src/main/java/com/bot/sup/repository/specification/ActivitySpecification.java
+++ src/main/java/com/bot/sup/repository/specification/ActivitySpecification.java
@@ -2,41 +2,26 @@ package com.bot.sup.repository.specification;
 
 import com.bot.sup.model.ActivityRequestParams;
 import com.bot.sup.model.dto.ActivityDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.List;
 
+@RequiredArgsConstructor
 public class ActivitySpecification implements Specification<ActivityDto> {
 
     private final ActivityRequestParams params;
 
-    public ActivitySpecification(ActivityRequestParams params) {
-        this.params = params;
-    }
-
     @Override
     public Predicate toPredicate(Root<ActivityDto> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        List<Predicate> predicates = new ArrayList<>();
-        if (params.getActivityTypeId() != null) {
-            predicates.add(
-                    builder.equal(root.get("activityType"), params.getActivityTypeId())
-            );
-        }
-        if (params.getActivityFormatId() != null) {
-            predicates.add(
-                    builder.equal(root.get("activityFormat"), params.getActivityFormatId())
-            );
-        }
-        if (params.getRouteId() != null) {
-            predicates.add(
-                    builder.equal(root.get("route"), params.getRouteId())
-            );
-        }
-        return builder.and(predicates.toArray(new Predicate[0]));
+        SpecificationBuilder<ActivityDto> specificationBuilder = new SpecificationBuilder<>(root, query, builder);
+        specificationBuilder.addEqualsPredicateIfNotNull("activityType", params.getActivityTypeId());
+        specificationBuilder.addEqualsPredicateIfNotNull("activityFormat", params.getActivityFormatId());
+        specificationBuilder.addEqualsPredicateIfNotNull("route", params.getRouteId());
+
+        return builder.and(specificationBuilder.build());
     }
 }
--- src/main/java/com/bot/sup/repository/specification/ScheduleSpecification.java
+++ src/main/java/com/bot/sup/repository/specification/ScheduleSpecification.java
@@ -2,36 +2,26 @@ package com.bot.sup.repository.specification;
 
 import com.bot.sup.model.ScheduleRequestParams;
 import com.bot.sup.model.dto.ScheduleDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.List;
 
+@RequiredArgsConstructor
 public class ScheduleSpecification implements Specification<ScheduleDto> {
     private final ScheduleRequestParams params;
 
-    public ScheduleSpecification(ScheduleRequestParams params) {
-        this.params = params;
-    }
 
     @Override
     public Predicate toPredicate(Root<ScheduleDto> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        List<Predicate> predicates = new ArrayList<>();
-        if (params.getActivityId() != null) {
-            predicates.add(
-                    builder.equal(root.get("activity"), params.getActivityId())
-            );
-        }
-        if (params.getRouteId() != null) {
-            predicates.add(
-                    builder.equal(root.get("route"), params.getRouteId())
-            );
-        }
 
-        return builder.and(predicates.toArray(new Predicate[0]));
+        SpecificationBuilder<ScheduleDto> specificationBuilder = new SpecificationBuilder<>(root, query, builder);
+        specificationBuilder.addEqualsPredicateIfNotNull("activity", params.getActivityId());
+        specificationBuilder.addEqualsPredicateIfNotNull("route", params.getRouteId());
+
+        return builder.and(specificationBuilder.build());
     }
 }
