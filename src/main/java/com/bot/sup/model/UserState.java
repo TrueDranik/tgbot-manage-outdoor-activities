beccf4052af52b1f06a8aa2d239471149dfc42a9
Ruslan N <>
2023-01-21 22:11:23 +0300

    fields changed

--- src/main/java/com/bot/sup/model/UserState.java
+++ src/main/java/com/bot/sup/model/UserState.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class UserState {
     private Long adminTelegramId;
-    private String state;
-    private Long instructorTelegramId;
+    private Enum<?> state;
+    private Object entity;
+    private boolean forUpdate;
 }
