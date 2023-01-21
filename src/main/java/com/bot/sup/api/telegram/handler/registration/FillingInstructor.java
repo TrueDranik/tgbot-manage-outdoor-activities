0bcc39fbfcbc9409dc54101f1d77c28dbf1c4278
Ruslan N <>
2023-01-21 22:17:25 +0300

    [BOTS-282] added cache usage

--- src/main/java/com/bot/sup/api/telegram/handler/registration/FillingInstructor.java
+++ src/main/java/com/bot/sup/api/telegram/handler/registration/FillingInstructor.java
@@ -2,8 +2,9 @@ package com.bot.sup.api.telegram.handler.registration;
 
 import com.bot.sup.api.telegram.handler.registration.instructor.InstructorMessageProcessor;
 import com.bot.sup.api.telegram.handler.registration.instructor.MessageProcessor;
-import com.bot.sup.cache.InstructorDataCache;
+import com.bot.sup.cache.UserStateCache;
 import com.bot.sup.common.enums.InstructorStateEnum;
+import com.bot.sup.model.UserState;
 import com.bot.sup.model.entity.Instructor;
 import com.bot.sup.repository.InstructorRepository;
 import com.bot.sup.service.instructor.InstructorService;
@@ -21,35 +22,39 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 public class FillingInstructor implements HandleRegistration {
-    private final InstructorDataCache instructorDataCache;
     private final InstructorService instructorService;
     private final InstructorRepository instructorRepository;
+    private final UserStateCache userStateCache;
     private final Map<InstructorStateEnum, InstructorMessageProcessor> instructorMessageProcessorMap;
 
     @Override
     public BotApiMethod<?> getMessage(Message message) {
         Instructor instructor;
         Long chatId = message.getChatId();
-        Long instructorForUpdateId = instructorDataCache.getInstructorForUpdate(chatId);
-        boolean forUpdate = instructorForUpdateId != null;
+        Long instructorForUpdateId = 0L;
+        UserState userState = userStateCache.getByTelegramId(chatId);
 
+        boolean forUpdate = userState.isForUpdate();
         if (forUpdate) {
             instructor = instructorRepository.findByTelegramId(instructorForUpdateId)
                     .orElseThrow(EntityNotFoundException::new);
 
             log.info("Found instructor with tgId - {} and name - {}", instructor.getTelegramId(), instructor.getFirstName());
         } else {
-            instructor = instructorDataCache.getInstructorProfileData(chatId);
+            instructor = (Instructor) userState.getEntity();
         }
-        if (instructorDataCache.getInstructorCurrentState(chatId).equals(InstructorStateEnum.FILLING_INSTRUCTOR))
-            instructorDataCache.setInstructorCurrentState(chatId, InstructorStateEnum.START_PROCESSING);
+
+        if (userState.getState().equals(InstructorStateEnum.FILLING_INSTRUCTOR)) {
+            userState.setState(InstructorStateEnum.START_PROCESSING);
+            userStateCache.createOrUpdateState(userState);
+        }
 
         return processInputMessage(message, chatId, instructor, forUpdate);
     }
 
     @Transactional
     public BotApiMethod<?> processInputMessage(Message inputMessage, Long chatId, Instructor instructor, boolean forUpdate) {
-        InstructorStateEnum instructorCurrentState = instructorDataCache.getInstructorCurrentState(chatId);
+        InstructorStateEnum instructorCurrentState = (InstructorStateEnum) userStateCache.getByTelegramId(chatId).getState();
         MessageProcessor messageProcessor = instructorMessageProcessorMap.get(instructorCurrentState);
 
         if (messageProcessor.isMessageInvalid(inputMessage)) {
@@ -59,7 +64,7 @@ public class FillingInstructor implements HandleRegistration {
         if (forUpdate) {
             instructorService.save(instructor);
         } else {
-            instructorDataCache.saveInstructorProfileData(chatId, instructor);
+            userStateCache.getByTelegramId(chatId).setEntity(instructor);
         }
 
         return messageProcessor.processInputMessage(inputMessage, instructor);
