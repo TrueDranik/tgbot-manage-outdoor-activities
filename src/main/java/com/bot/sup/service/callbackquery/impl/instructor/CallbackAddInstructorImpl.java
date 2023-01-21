52b1688bb6a905d3f77a6f8e705adde611b05010
Ruslan N <>
2023-01-21 22:17:08 +0300

    [BOTS-282] added cache usage

--- src/main/java/com/bot/sup/service/callbackquery/impl/instructor/CallbackAddInstructorImpl.java
+++ src/main/java/com/bot/sup/service/callbackquery/impl/instructor/CallbackAddInstructorImpl.java
@@ -1,10 +1,11 @@
 package com.bot.sup.service.callbackquery.impl.instructor;
 
 import com.bot.sup.api.telegram.handler.StateContext;
-import com.bot.sup.cache.InstructorDataCache;
-import com.bot.sup.cache.MiddlewareDataCache;
+import com.bot.sup.cache.UserStateCache;
 import com.bot.sup.common.enums.CallbackEnum;
 import com.bot.sup.common.enums.InstructorStateEnum;
+import com.bot.sup.model.UserState;
+import com.bot.sup.model.entity.Instructor;
 import com.bot.sup.service.callbackquery.Callback;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,24 +19,27 @@ import static com.bot.sup.common.enums.CallbackEnum.ADD_INSTRUCTOR;
 @Service
 public class CallbackAddInstructorImpl implements Callback {
     private final StateContext stateContext;
-    private final InstructorDataCache instructorDataCache;
-    private final MiddlewareDataCache middlewareDataCache;
-
-    public static final CallbackEnum ACTIVITIES = ADD_INSTRUCTOR;
+    private final UserStateCache userStateCache;
 
     @Override
     public PartialBotApiMethod<?> getCallbackQuery(CallbackQuery callbackQuery) throws TelegramApiException {
         Long chatId = callbackQuery.getMessage().getChatId();
-
         InstructorStateEnum botStateEnum = InstructorStateEnum.FILLING_INSTRUCTOR;
-        instructorDataCache.setInstructorCurrentState(chatId, botStateEnum);
-        middlewareDataCache.setValidCurrentState(chatId, botStateEnum);
+        Instructor instructor = new Instructor();
+
+        UserState userState = new UserState();
+        userState.setAdminTelegramId(chatId);
+        userState.setState(botStateEnum);
+        userState.setEntity(instructor);
+        userState.setForUpdate(false);
+
+        userStateCache.createOrUpdateState(userState);
 
         return stateContext.processInputMessage(botStateEnum, callbackQuery.getMessage());
     }
 
     @Override
     public CallbackEnum getSupportedActivities() {
-        return ACTIVITIES;
+        return ADD_INSTRUCTOR;
     }
 }
