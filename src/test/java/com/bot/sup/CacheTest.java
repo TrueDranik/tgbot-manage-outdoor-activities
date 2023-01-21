6a455a45f6f7eca214c2dc69ad98cfbb27250d52
Ruslan N <>
2023-01-21 22:12:43 +0300

    CacheTest

--- src/test/java/com/bot/sup/CacheTest.java
+++ src/test/java/com/bot/sup/CacheTest.java
@@ -1,7 +1,9 @@
 package com.bot.sup;
 
 import com.bot.sup.cache.UserStateCache;
+import com.bot.sup.common.enums.InstructorStateEnum;
 import com.bot.sup.model.UserState;
+import com.bot.sup.model.entity.Instructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +16,15 @@ class CacheTest extends AbstractTest {
 
     @Test
     void testCache() {
+        Object entity = new Instructor();
         UserState state = new UserState();
         state.setAdminTelegramId(432423432L);
-        state.setInstructorTelegramId(6546456L);
-        state.setState("START");
+        state.setEntity(entity);
+        state.setState(InstructorStateEnum.FILLING_INSTRUCTOR);
+
         stateCache.createOrUpdateState(state);
         stateCache.createOrUpdateState(state);
+
         UserState userState = stateCache.getByTelegramId(432423432L);
         log.info("Got user - {}", userState);
         stateCache.deleteFromCache(432423432L);
