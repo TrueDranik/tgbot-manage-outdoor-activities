4faa2d672eddfe8b5da13ce53d8a07adc9175d83
Ruslan N <>
2023-01-24 17:59:19 +0300

    refactored

--- src/main/java/com/bot/sup/api/telegram/handler/registration/MessageProcessor.java
+++ src/main/java/com/bot/sup/api/telegram/handler/registration/MessageProcessor.java
@@ -1,10 +1,8 @@
 package com.bot.sup.api.telegram.handler.registration;
 
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
-@Component
 public interface MessageProcessor {
     BotApiMethod<?> processInputMessage(Message message, Object entity);
 
--- src/main/java/com/bot/sup/api/telegram/handler/registration/instructor/InstructorMessageProcessor.java
+++ src/main/java/com/bot/sup/api/telegram/handler/registration/instructor/InstructorMessageProcessor.java
@@ -2,9 +2,7 @@ package com.bot.sup.api.telegram.handler.registration.instructor;
 
 import com.bot.sup.api.telegram.handler.registration.MessageProcessor;
 import com.bot.sup.common.enums.InstructorStateEnum;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface InstructorMessageProcessor extends MessageProcessor {
     InstructorStateEnum getCurrentState();
 }
