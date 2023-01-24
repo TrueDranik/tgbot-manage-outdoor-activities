f5be1d8491e0ed67d470288928f1ea8983f463d8
Ruslan N <>
2023-01-24 18:00:59 +0300

    implemented a change

--- src/main/java/com/bot/sup/service/callbackquery/impl/activity/format/CallbackActivityFormatChangeDataImpl.java
+++ src/main/java/com/bot/sup/service/callbackquery/impl/activity/format/CallbackActivityFormatChangeDataImpl.java
@@ -1,28 +1,37 @@
 package com.bot.sup.service.callbackquery.impl.activity.format;
 
 import com.bot.sup.api.telegram.handler.StateContext;
+import com.bot.sup.cache.UserStateCache;
 import com.bot.sup.common.enums.ActivityFormatStateEnum;
 import com.bot.sup.common.enums.CallbackEnum;
+import com.bot.sup.model.UserState;
+import com.bot.sup.model.entity.ActivityFormat;
+import com.bot.sup.service.activity.format.impl.ActivityFormatServiceImpl;
 import com.bot.sup.service.callbackquery.Callback;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import util.UserStateUtil;
 
 @Service
 @RequiredArgsConstructor
 public class CallbackActivityFormatChangeDataImpl implements Callback {
     private final StateContext stateContext;
+    private final ActivityFormatServiceImpl activityFormatService;
+    private final UserStateCache userStateCache;
 
     @Override
     public PartialBotApiMethod<?> getCallbackQuery(CallbackQuery callbackQuery) throws TelegramApiException {
         Long chatId = callbackQuery.getMessage().getChatId();
         String activityFormatId = callbackQuery.getData().split("/")[1];
-
         ActivityFormatStateEnum activityFormatStateEnum = ActivityFormatStateEnum.FILLING_ACTIVITY_FORMAT;
 
-        // TODO: 23.01.2023 Сделать обновление сущности
+        ActivityFormat activityFormat = activityFormatService.findActivityFormatById(Long.valueOf(activityFormatId));
+
+        UserState userState = UserStateUtil.getUserState(chatId, activityFormatStateEnum, activityFormat, true);
+        userStateCache.createOrUpdateState(userState);
 
         return stateContext.processInputMessage(activityFormatStateEnum, callbackQuery.getMessage());
     }
--- src/main/java/com/bot/sup/service/callbackquery/impl/activity/type/CallbackActivityTypeChangeDataImpl.java
+++ src/main/java/com/bot/sup/service/callbackquery/impl/activity/type/CallbackActivityTypeChangeDataImpl.java
@@ -1,28 +1,37 @@
 package com.bot.sup.service.callbackquery.impl.activity.type;
 
 import com.bot.sup.api.telegram.handler.StateContext;
+import com.bot.sup.cache.UserStateCache;
 import com.bot.sup.common.enums.ActivityTypeStateEnum;
 import com.bot.sup.common.enums.CallbackEnum;
+import com.bot.sup.model.UserState;
+import com.bot.sup.model.entity.ActivityType;
+import com.bot.sup.service.activity.type.impl.ActivityTypeServiceImpl;
 import com.bot.sup.service.callbackquery.Callback;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import util.UserStateUtil;
 
 @Service
 @RequiredArgsConstructor
 public class CallbackActivityTypeChangeDataImpl implements Callback {
     private final StateContext stateContext;
+    private final ActivityTypeServiceImpl activityTypeService;
+    private final UserStateCache userStateCache;
 
     @Override
     public PartialBotApiMethod<?> getCallbackQuery(CallbackQuery callbackQuery) throws TelegramApiException {
         Long chatId = callbackQuery.getMessage().getChatId();
         String activityTypeId = callbackQuery.getData().split("/")[1];
-
         ActivityTypeStateEnum activityTypeStateEnum = ActivityTypeStateEnum.FILLING_ACTIVITY_TYPE;
 
-        // TODO: 23.01.2023 сдедать обновление сущности
+        ActivityType activityType = activityTypeService.findActivityTypeById(Long.valueOf(activityTypeId));
+
+        UserState userState = UserStateUtil.getUserState(chatId, activityTypeStateEnum, activityType, true);
+        userStateCache.createOrUpdateState(userState);
 
         return stateContext.processInputMessage(activityTypeStateEnum, callbackQuery.getMessage());
     }
