7b00b61e155ae10aad455b3a17f329854356bed6
Ruslan N <>
2023-01-24 11:24:43 +0300

    the logic of instructor states is divided into classes

--- src/main/java/com/bot/sup/common/enums/InstructorStateEnum.java
+++ src/main/java/com/bot/sup/common/enums/InstructorStateEnum.java
@@ -3,9 +3,7 @@ package com.bot.sup.common.enums;
 public enum InstructorStateEnum {
     START_PROCESSING,
     ASK_FULL_NAME,
-    FILLING_FULL_NAME,
     ASK_PHONE_NUMBER,
     ASK_TELEGRAM_ID,
-    GET_TELEGRAM_ID,
     FILLING_INSTRUCTOR
 }
