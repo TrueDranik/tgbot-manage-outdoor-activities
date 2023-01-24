1e47c7d664f0e10bb561ea1ed63b41c26378faf3
Ruslan N <>
2023-01-24 17:59:00 +0300

    refactored

--- src/main/java/com/bot/sup/api/telegram/handler/registration/activity/type/ActivityTypeMessageProcessor.java
+++ src/main/java/com/bot/sup/api/telegram/handler/registration/activity/type/ActivityTypeMessageProcessor.java
@@ -2,9 +2,7 @@ package com.bot.sup.api.telegram.handler.registration.activity.type;
 
 import com.bot.sup.api.telegram.handler.registration.MessageProcessor;
 import com.bot.sup.common.enums.ActivityTypeStateEnum;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface ActivityTypeMessageProcessor extends MessageProcessor {
     ActivityTypeStateEnum getCurrentState();
 }
