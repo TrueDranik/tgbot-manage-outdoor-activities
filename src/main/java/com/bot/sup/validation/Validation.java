9e54d5117d9e392f2e9ab064d762307ea428430d
Ruslan N <>
2023-01-24 17:59:27 +0300

    refactored

--- src/main/java/com/bot/sup/validation/Validation.java
+++ src/main/java/com/bot/sup/validation/Validation.java
@@ -1,23 +1,25 @@
 package com.bot.sup.validation;
 
+import lombok.experimental.UtilityClass;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Validation { // TODO: 12.12.2022 мб назвать utils
-
+@UtilityClass
+public class Validation {
     private static final String REGEX_LETTERS = "^(?![\\d+_@.-]+$)[a-zA-Z а-яёА-ЯЁ+_@.-]*$";
     private static final String REGEX_PHONE = "^(\\+7)?[489][0-9]{2}[0-9]{3}[0-9]{2}[0-9]{2}$";
+    private static final Pattern PATTERN_NAME = Pattern.compile(REGEX_LETTERS);
+    private static final Pattern PATTERN_PHONE = Pattern.compile(REGEX_PHONE);
 
     public static boolean isValidText(String text) {
-        Pattern pattern = Pattern.compile(REGEX_LETTERS);
-        Matcher matcher = pattern.matcher(text);
+        Matcher matcher = PATTERN_NAME.matcher(text);
 
         return matcher.matches();
     }
 
     public static boolean isValidPhoneNumber(String phoneNumber) {
-        Pattern pattern = Pattern.compile(REGEX_PHONE);
-        Matcher matcher = pattern.matcher(phoneNumber);
+        Matcher matcher = PATTERN_PHONE.matcher(phoneNumber);
 
         return matcher.matches();
     }
