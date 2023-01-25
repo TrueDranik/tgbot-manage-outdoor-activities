cc8950c09b3357f66b32d23f6d2f34d8bc0aea31
Ruslan N <>
2023-01-25 18:00:44 +0300

    updated mappers

--- src/main/java/com/bot/sup/mapper/ActivityMapper.java
+++ src/main/java/com/bot/sup/mapper/ActivityMapper.java
@@ -1,9 +1,29 @@
 package com.bot.sup.mapper;
 
-import com.bot.sup.model.dto.ActivityCreateDto;
+import com.bot.sup.model.dto.ActivityDto;
 import com.bot.sup.model.entity.Activity;
+import com.bot.sup.repository.ActivityFormatRepository;
+import com.bot.sup.repository.ActivityTypeRepository;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityNotFoundException;
 
 @Mapper(componentModel = "spring")
-public interface ActivityMapper extends BaseMapper<Activity, ActivityCreateDto> {
+public abstract class ActivityMapper implements BaseMapper<Activity, ActivityDto> {
+    @Autowired
+    ActivityTypeRepository activityTypeRepository;
+    @Autowired
+    ActivityFormatRepository activityFormatRepository;
+
+    @AfterMapping
+    public void setRepoValues(ActivityDto dto, @MappingTarget Activity activity) {
+        activity.setIsActive(true);
+        activity.setActivityType(activityTypeRepository.findById(dto.getActivityType().getId())
+                .orElseThrow(() -> new EntityNotFoundException("Activity type not found")));
+        activity.setActivityFormat(activityFormatRepository.findById(dto.getActivityFormat().getId())
+                .orElseThrow(() -> new EntityNotFoundException("Activity format not found")));
+    }
 }
--- src/main/java/com/bot/sup/mapper/ActivityTypeMapper.java
+++ src/main/java/com/bot/sup/mapper/ActivityTypeMapper.java
@@ -1,10 +1,13 @@
 package com.bot.sup.mapper;
 
-import com.bot.sup.model.dto.ActivityTypeCreateDto;
+import com.bot.sup.model.dto.ActivityTypeDto;
 import com.bot.sup.model.entity.ActivityType;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface ActivityTypeMapper extends BaseMapper<ActivityType, ActivityTypeCreateDto> {
-
+public interface ActivityTypeMapper extends BaseMapper<ActivityType, ActivityTypeDto> {
+    @Override
+    @Mapping(target = "id", ignore = true)
+    ActivityType dtoToDomain(ActivityTypeDto activityTypeDto);
 }
