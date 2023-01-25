ef72f38f6bc70bf4231dab5b4e09d2129c3d1f01
Ruslan N <>
2023-01-25 18:04:32 +0300

    updated services

--- src/main/java/com/bot/sup/service/activity/type/impl/ActivityTypeServiceImpl.java
+++ src/main/java/com/bot/sup/service/activity/type/impl/ActivityTypeServiceImpl.java
@@ -1,15 +1,16 @@
 package com.bot.sup.service.activity.type.impl;
 
 import com.bot.sup.mapper.ActivityTypeMapper;
-import com.bot.sup.model.dto.ActivityTypeCreateDto;
+import com.bot.sup.model.dto.ActivityTypeDto;
 import com.bot.sup.model.entity.ActivityType;
+import com.bot.sup.model.entity.ActivityType_;
 import com.bot.sup.repository.ActivityTypeRepository;
 import com.bot.sup.service.activity.type.ActivityTypeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,40 +23,48 @@ public class ActivityTypeServiceImpl implements ActivityTypeService {
         activityTypeRepository.save(activityType);
     }
 
-    public boolean existsByNameEqualsIgnoreCase(String userAnswer){
+    public boolean existsByNameEqualsIgnoreCase(String userAnswer) {
         return activityTypeRepository.existsByNameEqualsIgnoreCase(userAnswer);
     }
 
     @Override
-    public List<ActivityType> getAllActivityType() {
-        return new ArrayList<>(activityTypeRepository.findAll());
+    public List<ActivityTypeDto> getAllActivityType() {
+        List<ActivityType> activityTypes = activityTypeRepository.findAll();
+
+        return activityTypeMapper.domainsToDtos(activityTypes);
     }
 
     @Override
-    public ActivityType getActivityTypeById(Long id) {
-        return findActivityTypeById(id);
-    }
-
-    @Override
-    public ActivityType createActivityType(ActivityTypeCreateDto activityTypeCreateDto) {
-        ActivityType activityType = activityTypeMapper.dtoToDomain(activityTypeCreateDto);
-
-        return activityTypeRepository.save(activityType);
-    }
-
-    @Override
-    public ActivityType updateActivityType(Long id, ActivityTypeCreateDto activityTypeCreateDto) {
+    public ActivityTypeDto getActivityTypeById(Long id) {
         ActivityType activityTypeById = findActivityTypeById(id);
 
-        activityTypeById.setName(activityTypeCreateDto.getName());
-        activityTypeById.setDescription(activityTypeCreateDto.getDescription());
+        return activityTypeMapper.domainToDto(activityTypeById);
+    }
 
-        return activityTypeRepository.save(activityTypeById);
+    @Override
+    public ActivityTypeDto createActivityType(ActivityTypeDto activityTypeDto) {
+        ActivityType activityType = activityTypeMapper.dtoToDomain(activityTypeDto);
+        activityTypeRepository.save(activityType);
+
+        return activityTypeMapper.domainToDto(activityType);
+    }
+
+    @Override
+    public ActivityTypeDto updateActivityType(Long id, ActivityTypeDto activityTypeDto) {
+        ActivityType activityTypeToUpdate = activityTypeMapper.dtoToDomain(activityTypeDto);
+        ActivityType activityTypeFromDb = findActivityTypeById(id);
+
+        BeanUtils.copyProperties(activityTypeToUpdate, activityTypeFromDb, ActivityType_.ID);
+        activityTypeRepository.save(activityTypeFromDb);
+
+        return activityTypeMapper.domainToDto(activityTypeFromDb);
     }
 
     @Override
     public void deleteActivityType(Long id) {
-        activityTypeRepository.delete(findActivityTypeById(id));
+        ActivityType activityTypeById = findActivityTypeById(id);
+
+        activityTypeRepository.delete(activityTypeById);
     }
 
     public ActivityType findActivityTypeById(Long id) {
